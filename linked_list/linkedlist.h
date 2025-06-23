d789c7b72821ad818760da7f9c826313aa1bf9d3
Polariusz <truepolariusz@gmail.com>
2025-06-23 20:09:45 +0200

    ll.h: feat: declared the header file

---
+++ linked_list/linkedlist.h
@@ -0,0 +1,36 @@
+#idndef LINKED_LIST_H_
+#define LINKED_LIST_H_
+
+typdef struct LinkedListNode {
+	void *item;
+	struct LinkedListNode *next;
+} LinkedListNode;
+
+typedef struct LinkedList {
+	LinkedListNode *root;
+	int cnt;
+	void(*dump_item)(void* item);
+} LinkedList;
+
+/* ---| CREATE |--- */
+int ll_new(LinkedList* ll, void(*dump_item)(void* item));
+int ll_new_ptr(LinkedList** ll, void(*dump_item)(void* item));
+
+/* ---| READ |--- */
+int ll_get(LinkedList *ll, void **dest, unsigned int idx);
+
+/* ---| UPDATE |--- */
+int ll_push(LinkedList *ll, void *item);
+int ll_insert(LinkedList *ll, void *item, unsigned int idx);
+int ll_replace(LinkedList *ll, void *item, unsigned int idx);
+
+/* ---| DELETE |--- */
+int ll_pop(LinkedList *ll, void **dest);
+int ll_delete(LinkedList *ll, void **dest, unsigned int idx);
+
+/* ---| CUSTOM |--- */
+int ll_dump(LinkedList *ll);
+int ll_destroy(LinkedList *ll);
+int ll_destroy_ptr(LinkedList *ll);
+
+#endif
