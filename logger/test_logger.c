b21641a9e62b7c2c8789263bad46be8a9c767981
Polariusz <truepolariusz@gmail.com>
2025-06-27 22:54:36 +0200

    logger.h: test: wrote test for  init and destructions of the logger

--- logger/test_logger.c
+++ logger/test_logger.c
@@ -4,22 +4,40 @@
 
 Test(get_log_type_as_str, equal_size_not_coloured)
 {
-	size_t header_len = strlen(get_log_type_as_str(DEBUG, 0));
+	size_t header_len = strlen(get_log_type_as_str(LOG_DEBUG, 0));
 
-	cr_assert_eq(header_len, strlen(get_log_type_as_str(FINE, 0)));
-	cr_assert_eq(header_len, strlen(get_log_type_as_str(WARN, 0)));
-	cr_assert_eq(header_len, strlen(get_log_type_as_str(ERROR, 0)));
-	cr_assert_eq(header_len, strlen(get_log_type_as_str(PANIC, 0)));
-	cr_assert_eq(header_len, strlen(get_log_type_as_str(PANIC+1, 0)));
+	cr_assert_eq(header_len, strlen(get_log_type_as_str(LOG_FINE, 0)));
+	cr_assert_eq(header_len, strlen(get_log_type_as_str(LOG_WARN, 0)));
+	cr_assert_eq(header_len, strlen(get_log_type_as_str(LOG_ERROR, 0)));
+	cr_assert_eq(header_len, strlen(get_log_type_as_str(LOG_PANIC, 0)));
+	cr_assert_eq(header_len, strlen(get_log_type_as_str(LOG_PANIC+1, 0)));
 }
 
 Test(get_log_type_as_str, equal_size_coloured)
 {
-	size_t header_len = strlen(get_log_type_as_str(DEBUG, 1));
+	size_t header_len = strlen(get_log_type_as_str(LOG_DEBUG, 1));
 
-	cr_assert_eq(header_len, strlen(get_log_type_as_str(FINE, 1)));
-	cr_assert_eq(header_len, strlen(get_log_type_as_str(WARN, 1)));
-	cr_assert_eq(header_len, strlen(get_log_type_as_str(ERROR, 1)));
-	cr_assert_eq(header_len, strlen(get_log_type_as_str(PANIC, 1)));
-	cr_assert_eq(header_len, strlen(get_log_type_as_str(PANIC+1, 1)));
+	cr_assert_eq(header_len, strlen(get_log_type_as_str(LOG_FINE, 1)));
+	cr_assert_eq(header_len, strlen(get_log_type_as_str(LOG_WARN, 1)));
+	cr_assert_eq(header_len, strlen(get_log_type_as_str(LOG_ERROR, 1)));
+	cr_assert_eq(header_len, strlen(get_log_type_as_str(LOG_PANIC, 1)));
+	cr_assert_eq(header_len, strlen(get_log_type_as_str(LOG_PANIC+1, 1)));
+}
+
+Test(logger, init_and_destroy)
+{
+	Logger logger;
+	int res = 0;
+	res = logger_new(&logger, "log.out", LOG_DEBUG, 1, 1); cr_assert(res == 0);
+
+	res = logger_destroy(&logger); cr_assert(res == 0);
+}
+
+Test(logger_ptr, init_and_destroy)
+{
+	Logger *logger;
+	int res = 0;
+	res = logger_new_ptr(&logger, "log.out", LOG_DEBUG, 1, 1); cr_assert(res == 0);
+
+	res = logger_destroy_ptr(&logger); cr_assert(res == 0);
 }
