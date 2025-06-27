176cabec5a13599a4348e69168c1374af2edca3e
Polariusz <truepolariusz@gmail.com>
2025-06-27 22:51:42 +0200

    logger.h: feat: redefined the logger header a bit. I also added the ifndef thingy cuz I forgor before

--- logger/logger.h
+++ logger/logger.h
@@ -1,25 +1,36 @@
+#ifndef LOGGER_H_
+#define LOGGER_H_
+
 #include <stdio.h>
 
 typedef enum LOG_TYPE {
-	DEBUG,
-	FINE,
-	WARN,
-	ERROR,
-	PANIC,
+	LOG_DEBUG,
+	LOG_FINE,
+	LOG_WARN,
+	LOG_ERROR,
+	LOG_PANIC,
 } LOG_TYPE;
+
+char *get_log_type_as_str(LOG_TYPE type, int is_coloured);
 
 typedef struct Logger {
 	FILE *fd;
 	LOG_TYPE log_from;
+	int colour;
+	int console_log;
 } Logger;
 
-void new_logger(Logger *l, LOG_TYPE log_from);
+int logger_message(Logger *l, LOG_TYPE type, char *msg);
+int logger_debug(Logger *l, char *msg);
+int logger_fine(Logger *l, char *msg);
+int logger_warn(Logger *l, char *msg);
+int logger_error(Logger *l, char *msg);
+int logger_panic(Logger *l, char *msg);
 
-char *get_log_type_as_str(LOG_TYPE type, int is_coloured);
+int logger_new(Logger *l, char *log_path, LOG_TYPE log_from, int colour, int console_log);
+int logger_new_ptr(Logger **l, char *log_path, LOG_TYPE log_from, int colour, int console_log);
 
-void log_message(Logger *l, LOG_TYPE type, char *msg);
-void log_debug(Logger *l, char *msg);
-void log_fine(Logger *l, char *msg);
-void log_warn(Logger *l, char *msg);
-void log_error(Logger *l, char *msg);
-void log_panic(Logger *l, char *msg);
+int logger_destroy(Logger *l);
+int logger_destroy_ptr(Logger **l);
+
+#endif
