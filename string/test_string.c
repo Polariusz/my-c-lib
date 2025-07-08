14d3e2dce1c2343c1fcfac7ac63a6a51db096316
Polariusz <truepolariusz@gmail.com>
2025-07-08 23:37:13 +0200

    string.h: test: renamed some tests and implemented tests for get_chars_slice

--- string/test_string.c
+++ string/test_string.c
@@ -1,7 +1,7 @@
 #include <criterion/criterion.h>
 #include "string.h"
 
-Test(init_and_destroy, simple)
+Test(init_and_destroy, good_simple)
 {
 	int res = 0;
 	String str;
@@ -10,7 +10,7 @@ Test(init_and_destroy, simple)
 	res = string_destroy(&str); cr_assert(res == 0);
 }
 
-Test(init_ptr_and_destroy, simple)
+Test(init_ptr_and_destroy, good_simple)
 {
 	int res = 0;
 	String *str;
@@ -20,7 +20,7 @@ Test(init_ptr_and_destroy, simple)
 	cr_assert(str == NULL);
 }
 
-Test(get_char, should_succeed)
+Test(get_char, good_simple)
 {
 	/* INIT */
 	int res = 0;
@@ -49,7 +49,7 @@ Test(get_char, should_succeed)
 	cr_assert(str == NULL);
 }
 
-Test(get_char, should_fail)
+Test(get_char, bad_out_of_bounds)
 {
 	/* INIT */
 	int res = 0;
@@ -58,8 +58,135 @@ Test(get_char, should_fail)
 
 	/* Main thing */
 	char minus = '-';
+	// Index 5 is out of bounds. Normal char* would have a NUL terminator there. The string that I've implemented does not have it.
+	// If you need it, use the string_get_chars_with_nul() function or the string_get_chars_slice_with_nul() function for more control.
 	res = string_get_char(str, 5, &minus); cr_assert(res != 0);
 	cr_assert_eq(minus, '-');
+
+	/* DETROY */
+	res = string_destroy_ptr(&str); cr_assert(res == 0);
+	cr_assert(str == NULL);
+}
+
+Test(get_chars_slice, good_slice)
+{
+	// The slice that the string_get_chars_slice() writes into the *dest does not have a NUL.
+	/* INIT */
+	int res = 0;
+	String *str;
+	res = string_new_ptr(&str, "Hello", 5); cr_assert(res == 0);
+
+	/* Main thing */
+	char dest[32] = {0};
+	res = string_get_chars_slice(str, 0, 4, dest); cr_assert(res == 0);
+	cr_assert_eq(dest[0], 'H');
+	cr_assert_eq(dest[1], 'e');
+	cr_assert_eq(dest[2], 'l');
+	cr_assert_eq(dest[3], 'l');
+	// The '4' is exclusive. I zero the memory for the dest with {0} (I think that this works this way), so dest[4] should never be 'o'.
+	cr_assert_neq(dest[4], 'o');
+
+	/* DETROY */
+	res = string_destroy_ptr(&str); cr_assert(res == 0);
+	cr_assert(str == NULL);
+}
+
+Test(get_chars_slice, good_full)
+{
+	/* INIT */
+	int res = 0;
+	String *str;
+	res = string_new_ptr(&str, "Hello", 5); cr_assert(res == 0);
+
+	/* Main thing */
+	char dest[32] = {0};
+	res = string_get_chars_slice(str, 0, 5, dest); cr_assert(res == 0);
+	cr_assert_eq(dest[0], 'H');
+	cr_assert_eq(dest[1], 'e');
+	cr_assert_eq(dest[2], 'l');
+	cr_assert_eq(dest[3], 'l');
+	cr_assert_eq(dest[4], 'o');
+
+	/* DETROY */
+	res = string_destroy_ptr(&str); cr_assert(res == 0);
+	cr_assert(str == NULL);
+}
+
+Test(get_chars_slice, bad_arg_to_out_of_bounds)
+{
+	/* INIT */
+	int res = 0;
+	String *str;
+	res = string_new_ptr(&str, "Hello", 5); cr_assert(res == 0);
+
+	/* Main thing */
+	char dest[32] = {0};
+	res = string_get_chars_slice(str, 0, 6, dest); cr_assert(res != 0);
+	cr_assert_neq(dest[0], 'H');
+	cr_assert_neq(dest[1], 'e');
+	cr_assert_neq(dest[2], 'l');
+	cr_assert_neq(dest[3], 'l');
+	cr_assert_neq(dest[4], 'o');
+
+	/* DETROY */
+	res = string_destroy_ptr(&str); cr_assert(res == 0);
+	cr_assert(str == NULL);
+}
+
+Test(get_chars_slice, bad_arg_from_out_of_bounds)
+{
+	/* INIT */
+	int res = 0;
+	String *str;
+	res = string_new_ptr(&str, "Hello", 5); cr_assert(res == 0);
+
+	/* Main thing */
+	char dest[32] = {0};
+	res = string_get_chars_slice(str, 6, 8, dest); cr_assert(res != 0);
+	cr_assert_neq(dest[0], 'H');
+	cr_assert_neq(dest[1], 'e');
+	cr_assert_neq(dest[2], 'l');
+	cr_assert_neq(dest[3], 'l');
+	cr_assert_neq(dest[4], 'o');
+
+	/* DETROY */
+	res = string_destroy_ptr(&str); cr_assert(res == 0);
+	cr_assert(str == NULL);
+}
+
+Test(get_chars_slice, bad_arg_from_equal_with_arg_to)
+{
+	/* INIT */
+	int res = 0;
+	String *str;
+	res = string_new_ptr(&str, "Hello", 5); cr_assert(res == 0);
+
+	/* Main thing */
+	char dest[32] = {0};
+	res = string_get_chars_slice(str, 3, 3, dest); cr_assert(res != 0);
+	cr_assert_neq(dest[0], 'H');
+	cr_assert_neq(dest[1], 'e');
+	cr_assert_neq(dest[2], 'l');
+	cr_assert_neq(dest[3], 'l');
+	cr_assert_neq(dest[4], 'o');
+
+	/* DETROY */
+	res = string_destroy_ptr(&str); cr_assert(res == 0);
+	cr_assert(str == NULL);
+}
+
+Test(get_chars_slice, good_single_char)
+{
+	/* INIT */
+	int res = 0;
+	String *str;
+	res = string_new_ptr(&str, "Hello", 5); cr_assert(res == 0);
+
+	/* Main thing */
+	char dest[32] = {0};
+	res = string_get_chars_slice(str, 1, 2, dest); cr_assert(res == 0);
+	cr_assert_eq(dest[0], 'e');
+	cr_assert_neq(dest[1], 'l');
 
 	/* DETROY */
 	res = string_destroy_ptr(&str); cr_assert(res == 0);
