dae9221a9d7d091a2009e983c81ed0ff5f8f3c35
Polariusz <truepolariusz@gmail.com>
2025-07-12 12:25:39 +0200

    hash.h: feat: added enum type to HashOpt to signify which hash function should be used and implemented hash_value that uses the enum for the right hash function call

--- hash/hash.c
+++ hash/hash.c
@@ -1,48 +1,72 @@
+enum HashType {
+	HASH_CHAR,
+	HASH_SHORT,
+	HASH_INT,
+	HASH_LONG,
+	HASH_LONG_LONG,
+	HASH_CSTRING,
+};
+
 typedef struct HashOpt {
-	unsigned int initial_value;
-	unsigned int sub_divider;
+	unsigned int initial_value; unsigned int sub_divider;
 	unsigned int divider;
+	enum HashType hash_type;
 } HashOpt;
 
-unsigned int hash_single(HashOpt *opt, unsigned long long key)
+unsigned int hash_as_char(HashOpt *opt, unsigned char key)
 {
 	return ((opt->initial_value + key) % opt->sub_divider) % opt->divider;
 }
 
-unsigned int hash_char(HashOpt *opt, unsigned char key)
+unsigned int hash_as_short(HashOpt *opt, unsigned short key)
 {
-	return hash_single(opt, (unsigned long long)key);
+	return ((opt->initial_value + key) % opt->sub_divider) % opt->divider;
 }
 
-unsigned int hash_short(HashOpt *opt, unsigned short key)
+unsigned int hash_as_int(HashOpt *opt, unsigned int key)
 {
-	return hash_single(opt, (unsigned long long)key);
+	return ((opt->initial_value + key) % opt->sub_divider) % opt->divider;
 }
 
-unsigned int hash_int(HashOpt *opt, unsigned int key)
+unsigned int hash_as_long(HashOpt *opt, unsigned long key)
 {
-	return hash_single(opt, (unsigned long long)key);
+	return ((opt->initial_value + key) % opt->sub_divider) % opt->divider;
 }
 
-unsigned int hash_long(HashOpt *opt, unsigned long key)
+unsigned int hash_as_long_long(HashOpt *opt, unsigned long long key)
 {
-	return hash_single(opt, (unsigned long long)key);
+	return ((opt->initial_value + key) % opt->sub_divider) % opt->divider;
 }
 
-unsigned int hash_long_long(HashOpt *opt, unsigned long long key)
-{
-	return hash_single(opt, key);
-}
-
-unsigned int hash_cstring(HashOpt *opt, char *key, unsigned int len)
+unsigned int hash_as_cstring(HashOpt *opt, char* key, unsigned int len)
 {
 	unsigned int res = opt->initial_value;
 
 	while(*key != '\0' && len > 0) {
-		res = (res + ((unsigned int)*key)) % opt->sub_divider;
+		res = (res + (*key)) % opt->sub_divider;
 		--len;
 		++key;
 	}
 
 	return res % opt->divider;
+}
+
+unsigned int hash_value(HashOpt *opt, void *key, unsigned int len)
+{
+	switch(opt->hash_type) {
+		case HASH_CHAR:
+			return hash_as_char(opt, *(unsigned char*)key);
+		case HASH_SHORT:
+			return hash_as_short(opt, *(unsigned short*)key);
+		case HASH_INT:
+			return hash_as_int(opt, *(unsigned int*)key);
+		case HASH_LONG:
+			return hash_as_long(opt, *(unsigned long*)key);
+		case HASH_LONG_LONG:
+			return hash_as_long_long(opt, *(unsigned long long*)key);
+		case HASH_CSTRING:
+			return hash_as_cstring(opt, (char*)key, len);
+	}
+
+	return -1;
 }
--- hash/hash.h
+++ hash/hash.h
@@ -1,28 +1,42 @@
 #ifndef HASH_H_
 #define HASH_H_
 
+enum HashType {
+	HASH_CHAR,
+	HASH_SHORT,
+	HASH_INT,
+	HASH_LONG,
+	HASH_LONG_LONG,
+	HASH_CSTRING,
+};
+
 typedef struct HashOpt {
 	unsigned int initial_value;
 	unsigned int sub_divider;
 	unsigned int divider;
+	enum HashType hash_type;
 } HashOpt;
 
-__attribute__((nonnull (1)))
-	unsigned int hash_char(HashOpt *opt, unsigned char key);
 
-__attribute__((nonnull (1)))
-	unsigned int hash_short(HashOpt *opt, unsigned short key);
+__attribute__ ((nonnull (1)))
+	unsigned int hash_as_char(HashOpt *opt, unsigned char key);
 
-__attribute__((nonnull (1)))
-	unsigned int hash_int(HashOpt *opt, unsigned int key);
+__attribute__ ((nonnull (1)))
+	unsigned int hash_as_short(HashOpt *opt, unsigned short key);
 
-__attribute__((nonnull (1)))
-	unsigned int hash_long(HashOpt *opt, unsigned long key);
+__attribute__ ((nonnull (1)))
+	unsigned int hash_as_int(HashOpt *opt, unsigned int key);
 
-__attribute__((nonnull (1)))
-	unsigned int hash_long_long(HashOpt *opt, unsigned long long key);
+__attribute__ ((nonnull (1)))
+	unsigned int hash_as_long(HashOpt *opt, unsigned long key);
 
-__attribute__ ((nonnull (1), access (read_only, 2, 3)))
-	unsigned int hash_cstring(HashOpt *opt, char *key, unsigned int len);
+__attribute__ ((nonnull (1)))
+	unsigned int hash_as_long_long(HashOpt *opt, unsigned long long key);
+
+__attribute__ ((nonnull (1)))
+	unsigned int hash_as_cstring(HashOpt *opt, char* key, unsigned int len);
+
+__attribute__ ((nonnull (1, 2), access (read_only, 2, 3)))
+	unsigned int hash_value(HashOpt *opt, void *key, unsigned int len);
 
 #endif
