dfda96e309b31ac35748267df73c505874ac0ac6
Polariusz <truepolariusz@gmail.com>
2025-07-14 17:26:59 +0200

    del: removed all functions except of modified hash_str and hash_strn. unsigned char *key is now void *key and hash_str now has argument len, which makes the functions have same args

--- hash/hash.c
+++ hash/hash.c
@@ -1,92 +1,30 @@
-enum HashType {
-	HASH_CHAR,
-	HASH_SHORT,
-	HASH_INT,
-	HASH_LONG,
-	HASH_LONG_LONG,
-	HASH_STR,
-	HASH_STRN,
-	HASH_CUSTOM,
-};
-
 typedef struct HashOpt {
 	unsigned int initial_value;
 	unsigned int sub_divider;
 	unsigned int divider;
-	enum HashType hash_type;
-	unsigned int (*hash_custom)(struct HashOpt *opt, void *key, unsigned int len);
 } HashOpt;
 
-
-unsigned int hash_char(HashOpt *opt, unsigned char key)
+unsigned int hash_str(HashOpt *opt, void *key, unsigned int len)
 {
-	return ((opt->initial_value + key) % opt->sub_divider) % opt->divider;
-}
-
-unsigned int hash_short(HashOpt *opt, unsigned short key)
-{
-	return ((opt->initial_value + key) % opt->sub_divider) % opt->divider;
-}
-
-unsigned int hash_int(HashOpt *opt, unsigned int key)
-{
-	return ((opt->initial_value + key) % opt->sub_divider) % opt->divider;
-}
-
-unsigned int hash_long(HashOpt *opt, unsigned long key)
-{
-	return ((opt->initial_value + key) % opt->sub_divider) % opt->divider;
-}
-
-unsigned int hash_long_long(HashOpt *opt, unsigned long long key)
-{
-	return ((opt->initial_value + key) % opt->sub_divider) % opt->divider;
-}
-
-unsigned int hash_str(HashOpt *opt, unsigned char* key)
-{
+	(void)len;
 	unsigned int res = opt->initial_value;
 
-	while(*key != '\0') {
-		res = (res + (*key)) % opt->sub_divider;
+	while(*(char*)key != '\0') {
+		res = (res + (*(char*)key)) % opt->sub_divider;
 		++key;
 	}
 
 	return res % opt->divider;
 }
 
-unsigned int hash_strn(HashOpt *opt, unsigned char* key, unsigned int len)
+unsigned int hash_strn(HashOpt *opt, void *key, unsigned int len)
 {
 	unsigned int res = opt->initial_value;
 	while(len > 0) {
-		res = (res + (*key)) % opt->sub_divider;
+		res = (res + (*(char*)key)) % opt->sub_divider;
 		--len;
 		++key;
 	}
 
 	return res % opt->divider;
-}
-
-unsigned int hash_value(HashOpt *opt, void *key, unsigned int len)
-{
-	switch(opt->hash_type) {
-		case HASH_CHAR:
-			return hash_char(opt, *(unsigned char*)key);
-		case HASH_SHORT:
-			return hash_short(opt, *(unsigned short*)key);
-		case HASH_INT:
-			return hash_int(opt, *(unsigned int*)key);
-		case HASH_LONG:
-			return hash_long(opt, *(unsigned long*)key);
-		case HASH_LONG_LONG:
-			return hash_long_long(opt, *(unsigned long long*)key);
-		case HASH_STR:
-			return hash_str(opt, (unsigned char*)key);
-		case HASH_STRN:
-			return hash_strn(opt, (unsigned char*)key, len);
-		case HASH_CUSTOM:
-			return opt->hash_custom(opt, key, len);
-	}
-
-	return -1;
 }
--- hash/hash.h
+++ hash/hash.h
@@ -1,48 +1,18 @@
 #ifndef HASH_H_
 #define HASH_H_
 
-enum HashType {
-	HASH_CHAR,
-	HASH_SHORT,
-	HASH_INT,
-	HASH_LONG,
-	HASH_LONG_LONG,
-	HASH_STR,
-	HASH_STRN,
-	HASH_CUSTOM,
-};
-
 typedef struct HashOpt {
 	unsigned int initial_value;
 	unsigned int sub_divider;
 	unsigned int divider;
-	enum HashType hash_type;
-	unsigned int (*hash_custom)(struct HashOpt *opt, void *key, unsigned int len);
 } HashOpt;
 
+// The argument len is not used, but is in the function to conform to the argument structure. The hashing will stop if the char will encounter a NUL Terminator.
+__attribute__ ((nonnull (1, 2), access (read_only, 2)))
+	unsigned int hash_str(HashOpt *opt, void *key, unsigned int len);
 
-__attribute__ ((nonnull (1)))
-	unsigned int hash_char(HashOpt *opt, unsigned char key);
-
-__attribute__ ((nonnull (1)))
-	unsigned int hash_short(HashOpt *opt, unsigned short key);
-
-__attribute__ ((nonnull (1)))
-	unsigned int hash_int(HashOpt *opt, unsigned int key);
-
-__attribute__ ((nonnull (1)))
-	unsigned int hash_long(HashOpt *opt, unsigned long key);
-
-__attribute__ ((nonnull (1)))
-	unsigned int hash_long_long(HashOpt *opt, unsigned long long key);
-
-__attribute__ ((nonnull (1, 2)))
-	unsigned int hash_str(HashOpt *opt, unsigned char* key);
-
+// NUL Terminator is ignored in the void *. Instead, the hash is generated from the pointer to len with chars.
 __attribute__ ((nonnull (1, 2), access (read_only, 2, 3)))
-	unsigned int hash_strn(HashOpt *opt, unsigned char* key, unsigned int len);
-
-__attribute__ ((nonnull (1, 2), access (read_only, 2, 3)))
-	unsigned int hash_value(HashOpt *opt, void *key, unsigned int len);
+	unsigned int hash_strn(HashOpt *opt, void *key, unsigned int len);
 
 #endif
