79b0b7013a80065d58f71abfad33192ffd2f6b5d
Polariusz <truepolariusz@gmail.com>
2025-07-16 16:45:55 +0200

    hm.h: feat: added an example c file

---
+++ hash_map/example.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include "hash_map.h"
+#include "../generic_functions/generic_functions.h" // It has the gf_default() function
+
+int main(void)
+{
+	// Declare and initialise new Hash Map:
+	HashMap hm;
+	hm_new(&hm, gf_default(), 1000);
+
+	// Add few key vals
+	KeyVal kv1 = {"Key", "one", 4, 4};
+	hm_add(&hm, &kv1);
+
+	KeyVal kv2 = {"Oraoraora", "Jot", 10, 4};
+	hm_add(&hm, &kv2);
+
+	// We can retrieve values as such:
+	KeyVal kvYoink1 = {"Key", NULL, 4, 0};
+	hm_get(&hm, &kvYoink1);
+
+	// kvYoink.val == "one"
+	printf("Key: '%s', Val: '%s'\n", (char*)kvYoink1.key, (char*)kvYoink1.val);
+
+	KeyVal kvYoink2 = {"Oraoraora", NULL, 10, 0};
+	hm_get(&hm, &kvYoink2);
+
+	// kvYoink.val == "Jot"
+	printf("Key: '%s', Val: '%s'\n", (char*)kvYoink2.key, (char*)kvYoink2.val);
+
+	// Note that adding the same key will result in an err:
+	KeyVal kv3 = {"Key", "foo", 4, 4};
+	if(int err = hm_add(&hm, &kv3); err != 0)
+		printf("Key: '%s' is already in the hash map.\n", (char*)kv3.key);
+
+	// Note that trying to get a value from a non existing key will result in an err:
+	KeyVal kv4 = {"Dio Brando", NULL, 10, 0};
+	if(int err = hm_get(&hm, &kv4); err != 0)
+		printf("Key: '%s' does not exists.\n", (char*)kv4.key);
+
+	// We can delete an entry as such:
+	KeyVal kvYoink3 = {"Key", NULL, 4, 0};
+	hm_delete(&hm, &kvYoink3);
+	// The deletion writes the val and val_len into the KeyVal argument.
+
+	// Now getting the entry with key "Key" will result in an err:
+	if(int err = hm_get(&hm, &kvYoink3); err != 0)
+		printf("Key: '%s' does not exists.\n", (char*)kvYoink3.key);
+
+	// HashMap destructor
+	hm_destroy(&hm);
+
+	printf("Just Monika\n");
+	return 0;
+}
