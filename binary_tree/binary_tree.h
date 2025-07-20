e16ad6b6f1aabc37f6fd6a4a80280d0ab763efc1
Polariusz <truepolariusz@gmail.com>
2025-07-20 23:00:27 +0200

    bt.h: feat: defined header file

---
+++ binary_tree/binary_tree.h
@@ -0,0 +1,40 @@
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+
+typedef struct BinaryTreeNode {
+	void *item;
+	struct BinaryTreeNode *left;
+	struct BinaryTreeNode *right;
+} BinaryTreeNode;
+
+typedef struct BinaryTree {
+	BinaryTreeNote *root;
+	unsigned int cnt;
+} BinaryTree;
+
+/* ---| CONSTRUCTOR |--- */
+int bt_new(BinaryTree *bt);
+int bt_new_ptr(BinaryTree **bt);
+
+/* ---| DESTRUCTOR |--- */
+int bt_destroy(BinaryTree *bt);
+int bt_destroy_ptr(BinaryTree **bt);
+
+/* ---| CUSTOM |--- */
+int bt_dump(BinaryTree *bt, void(*dump_item)(void *src));
+
+/* ---| CREATE |--- */
+int bt_add(BinaryTree *bt, void *item);
+
+/* ---| READ |--- */
+int bt_get(BinaryTree *bt, unsigned int idx, void **dest);
+
+/* ---| UPDATE |--- */
+int bt_swap_nodes(BinaryTree *bt, unsigned int idx);
+int bt_replace(BinaryTree *bt, unsigned int idx, void *item);
+
+/* ---| DELETE |--- */
+int bt_delete_node(BinaryTree *bt, void *item);
+int bt_delete_nodes(BinaryTree *bt, void *item);
+
+#endif
